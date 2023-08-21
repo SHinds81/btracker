e3222844daedb2168c5285a0673f8cc469f9a251
Sean Hinds <seanhinds81@gmail.com>
2023-08-21 20:58:43 +1000

    very beginning dev

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Topnav from './nav/Topnav'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Topnav />
     <App />
   </React.StrictMode>
 );
---
+++ src/nav/Topnav.js
@@ -0,0 +1,13 @@
+import '../index.css';
+import './Topnav.css';
+
+function Topnav(){
+    return(
+        <div className='Topnav'>
+            <header className='App-header'>Topnav</header>
+            <header className='Topnav-Header'>Topnav 2</header>
+        </div>
+    )
+}
+
+export default Topnav;
